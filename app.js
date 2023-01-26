24405acc38d16e37903dc104aa8db2e475a18068
bricheek <bricheek@gmail.com>
2023-01-26 17:54:17 -0500

    adding funcionality

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var items = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    var today = new Date();
+    var options = { weekday: 'long', 
+                    day: 'numeric', 
+                    month: 'long'
+    };
+    var day = today.toLocaleDateString('en-US', options);
+
+    res.render('list',{kindOfDay: day, newItems: items});
+});
+
+app.post('/', function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000!');
+});
